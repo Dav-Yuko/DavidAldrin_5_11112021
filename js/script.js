f61164f3fc62f6eba1403b93f032963823ab1382
David Aldrin <d.aldrin@outlook.fr>
2021-11-24 14:47:22 +0100

    First Commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*  atteint le catalogue sur url API  */
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    return displayProducts(data);
+  })
+  .catch(function (error) {
+    alert(error);
+  });
+
+//  récupère les données du catalogue sur url API
+function displayProducts(data) {
+  for (let element of data) {
+    makeArticles(element);
+  }
+}
+// affiche les produits sur la page d'accueil index.html
+function makeArticles(sofa) {
+  const article = `
+    <a href="./product.html?id=${sofa._id}">
+        <article>
+        <img src="${sofa.imageUrl}" alt="${sofa.altTxt}">
+        <h3 class="productName">${sofa.name}</h3>
+        <p class="productDescription">${sofa.description}</p>
+        </article>
+    </a>`;
+  document.getElementById("items").innerHTML += article;
+}
+// altTxt: "Photo d'un canapé bleu, deux places"
+// colors: (3) ['Blue', 'White', 'Black']
+// description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// imageUrl: "http://localhost:3000/images/kanap01.jpeg"
+// name: "Kanap Sinopé"
+// price: 1849
+// _id: "107fb5b75607497b96722bda5b504926"
